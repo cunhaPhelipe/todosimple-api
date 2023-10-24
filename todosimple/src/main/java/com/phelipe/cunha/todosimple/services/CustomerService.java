1d85fda92901ab3b98327609537cab79b1bdb056
Phelipe Cunha <cunha.phelipe.santos@gmail.com>
2023-10-24 00:33:29 -0300

    Construindo CustomerService

---
+++ todosimple/src/main/java/com/phelipe/cunha/todosimple/services/CustomerService.java
@@ -0,0 +1,34 @@
+package com.phelipe.cunha.todosimple.services;
+import com.phelipe.cunha.todosimple.models.Customer;
+import com.phelipe.cunha.todosimple.repositores.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+public class CustomerService {
+
+	@Autowired
+	private CustomerRepository customerRepository;
+
+	public Customer findById(Long id){
+		Optional<Customer> customer = this.customerRepository.findById(id);
+		return customer.orElseThrow(() -> new RuntimeException("Cliente não encontrado! Id: " + id + ", Tipo: " + Customer.class.getName()));
+	}
+
+	@Transactional
+	public Customer create(Customer customer){
+		customer = this.customerRepository.save(customer);
+		return customer;
+	}
+
+	@Transactional
+	public Customer update(Customer customer){
+		Customer newObj = findById(customer.getId());
+		newObj.setName(customer.getName());
+		newObj.setEndereco(customer.getEndereco());
+		newObj.setComplemento(customer.getComplemento());
+		return this.customerRepository.save(newObj);
+	}
+}
