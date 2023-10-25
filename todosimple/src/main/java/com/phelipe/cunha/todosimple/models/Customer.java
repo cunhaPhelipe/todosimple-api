96f8d7b4ec6cb9181a249fcb6d3d27fc34d464b6
Phelipe Cunha <cunha.phelipe.santos@gmail.com>
2023-10-24 22:57:14 -0300

    Inserindo colunas de cep e numero no banco de dados

--- todosimple/src/main/java/com/phelipe/cunha/todosimple/models/Customer.java
+++ todosimple/src/main/java/com/phelipe/cunha/todosimple/models/Customer.java
@@ -1,9 +1,7 @@
 package com.phelipe.cunha.todosimple.models;
 import javax.persistence.Entity;
 import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @Entity
 @Table(name = Customer.TABLE_NAME)
@@ -47,22 +45,30 @@ public class Customer {
 		this.complemento = complemento;
 	}
 
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", unique = true)
 	private Long id;
 
-	@Column(name = "name", length = 100, nullable = false)
+	@Column(name = "name", length = 255, nullable = false)
 	@NotNull
 	@NotEmpty
-	@Size(min = 2, max = 30)
+	@Size(min = 1, max = 255)
 	private  String name;
 
 
-	@Column(name = "endereco", length = 100, nullable = false)
+	@Column(name = "endereco", length = 255, nullable = false)
 	@NotNull
 	@NotEmpty
-	@Size(min = 2, max =  100)
+	@Size(min = 2, max =  255)
 	private String endereco;
 
 
@@ -73,4 +79,21 @@ public class Customer {
 	private String complemento;
 
 
+	@Column(name = "numero", length = 40, nullable = true)
+	@Digits(integer = 10,fraction=0)
+	private String numero;
+
+	@Column(name = "cep", length = 100, nullable = true)
+	@NotNull
+	@NotEmpty
+	@Size(min = 8, max = 12, message = "Informe o CEP sem o digito")
+	private String cep;
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
 }
